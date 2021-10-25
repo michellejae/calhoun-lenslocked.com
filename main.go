e6781512af3d9ed5aecad906ab3f7c5207dc3bf9
michelle jae <moyer.mj@gmail.com>
2021-10-25 12:17:21 -0400

    first router

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1> Welcome to my site </h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1> Welcome to my site </h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprintf(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	}
+
 }
 
 func main() {
