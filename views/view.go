863fd0b06c1194d25dfd0a0c78159ece7255af4a
michelle jae <moyer.mj@gmail.com>
2021-10-26 23:27:48 -0400

    nav bar

--- views/view.go
+++ views/view.go
@@ -5,7 +5,10 @@ import "html/template"
 //creates new view object, parses all the template files given, and returns what is necessary to us as View type
 func NewView(layout string, files ...string) *View {
 	// appends all files we provide to this func to the files arg in this case it's our template for footer, may also need one for header, etc. may change for each section
-	files = append(files, "views/layouts/footer.gohtml", "views/layouts/bootstrap.gohtml")
+	files = append(files,
+		"views/layouts/footer.gohtml",
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/navbar.gohtml")
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
