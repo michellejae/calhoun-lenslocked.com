469511dadc98d4b96fe14bbbef2f4736f41e8303
michelle jae <moyer.mj@gmail.com>
2021-10-26 23:40:47 -0400

    refactored view.go template schtuff

--- views/view.go
+++ views/view.go
@@ -1,15 +1,20 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+// created variables to use in func layoutFiles so we don't have to hardcode into Glob
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
 
 //creates new view object, parses all the template files given, and returns what is necessary to us as View type
 func NewView(layout string, files ...string) *View {
 	// appends all files we provide to this func to the files arg in this case it's our template for footer, may also need one for header, etc. may change for each section
-	files = append(files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml")
-
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -25,4 +30,14 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+// layout files returns a slice of stirngs representing
+// the layout files used in our application
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
