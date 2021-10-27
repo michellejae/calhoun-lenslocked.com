e5f73e4b62b4aeaa5482a1f4751b16ee5bb034dc
michelle jae <moyer.mj@gmail.com>
2021-10-26 23:08:13 -0400

    added boostrap template

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	// uses the homeView type (which is a Template type) to execute
-	err := homeView.Template.Execute(w, nil)
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -25,7 +25,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := contactView.Template.Execute(w, nil)
+	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -36,8 +36,8 @@ func main() {
 	// this way we know right away if we have issues with our html pages (the errors/panic happen on view.go)
 	// vs waiting until someone hits the page and then finding out there we have error from our templates
 	// they will not be "execute/render" until each page is hit, which is fine.
-	homeView = views.NewView("views/home.gohtml")
-	contactView = views.NewView("views/contact.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
--- views/view.go
+++ views/view.go
@@ -3,9 +3,9 @@ package views
 import "html/template"
 
 //creates new view object, parses all the template files given, and returns what is necessary to us as View type
-func NewView(files ...string) *View {
+func NewView(layout string, files ...string) *View {
 	// appends all files we provide to this func to the files arg in this case it's our template for footer, may also need one for header, etc. may change for each section
-	files = append(files, "views/layouts/footer.gohtml")
+	files = append(files, "views/layouts/footer.gohtml", "views/layouts/bootstrap.gohtml")
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -15,9 +15,11 @@ func NewView(files ...string) *View {
 	}
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 }
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }
