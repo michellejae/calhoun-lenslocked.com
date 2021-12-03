bdc9747811e0d5ec68d898953c0e3b8967594fc3
michelle jae <moyer.mj@gmail.com>
2021-12-03 18:57:27 -0500

    added user service and create method to contorller

--- controllers/users.go
+++ controllers/users.go
@@ -4,19 +4,22 @@ import (
 	"fmt"
 	"net/http"
 
+	"gitlab.com/michellejae/lenslocked.com/models"
 	"gitlab.com/michellejae/lenslocked.com/views"
 )
 
 // NewUsers is used to create a new Users Controller
 //this function will panic if templates are not parsed correctly and should only be used during initial setup
-func NewUsers() *Users {
+func NewUsers(us *models.UserService) *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "users/new"),
+		us:      us,
 	}
 }
 
 type Users struct {
 	NewView *views.View
+	us      *models.UserService
 }
 
 // New is used to render the form where a user can create a new user account
@@ -39,6 +42,14 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var form SignupForm
 	if err := u.parseForm(r, &form); err != nil {
 		panic(err)
+	}
+	user := models.User{
+		Name:  "",
+		Email: form.Email,
+	}
+	if err := u.us.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	fmt.Fprintln(w, form)
 
--- main.go
+++ main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"gitlab.com/michellejae/lenslocked.com/controllers"
+	"gitlab.com/michellejae/lenslocked.com/models"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "macbookprowoe"
+	dbname = "lenslocked_dev"
 )
 
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, port, user, dbname)
+
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
 
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	usersC := controllers.NewUsers(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
 	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+	fmt.Println("Starting the server on :3000")
 	http.ListenAndServe(":3000", r)
 }
