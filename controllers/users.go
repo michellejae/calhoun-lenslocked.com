492f497486d62e622dd60a4aecf0047ed1be995c
michelle jae <moyer.mj@gmail.com>
2021-12-06 15:05:12 -0500

    udapted the users controller to set and recall the remembered_token for signin

--- controllers/users.go
+++ controllers/users.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"gitlab.com/michellejae/lenslocked.com/models"
+	"gitlab.com/michellejae/lenslocked.com/rand"
 	"gitlab.com/michellejae/lenslocked.com/views"
 )
 
@@ -57,7 +58,11 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// have to use a pointer cause above user is regular user variable
-	signIn(w, &user)
+	err := u.signIn(w, &user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	http.Redirect(w, r, "/cookietest", http.StatusFound)
 }
 
@@ -91,26 +96,48 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// user is already a pointre cause that's what returned by authenticate method
-	signIn(w, user)
+	err = u.signIn(w, user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	http.Redirect(w, r, "/cookietest", http.StatusFound)
 
 }
 
 // used to sign the given user in via cookies
-func signIn(w http.ResponseWriter, user *models.User) {
+func (u *Users) signIn(w http.ResponseWriter, user *models.User) error {
+	if user.Remember == "" {
+		token, err := rand.RememberToken()
+		if err != nil {
+			return err
+		}
+		user.Remember = token
+		err = u.us.Update(user)
+		if err != nil {
+			return err
+		}
+	}
 	cookie := http.Cookie{
-		Name:  "email",
-		Value: user.Email,
+		Name:  "remember_token",
+		Value: user.Remember,
 	}
 	http.SetCookie(w, &cookie)
+
+	return nil
 }
 
 //used to display cookies set on current user (only use during development, delete for productions)
 func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("email")
+	cookie, err := r.Cookie("remember_token")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintln(w, "Email is:", cookie.Value)
+	user, err := u.us.ByRemember(cookie.Value)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, user)
 }
