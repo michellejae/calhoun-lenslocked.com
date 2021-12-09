758bdfa402a3a111e8908b519d3c7449c0bcadc7
michelle jae <moyer.mj@gmail.com>
2021-12-09 17:36:41 -0500

    implemented the hmacRemember normlizer that normalizers the remember token

--- models/users.go
+++ models/users.go
@@ -147,15 +147,18 @@ type userValidator struct {
 // ByRemember will hash the remember token and then call ByRemember on the subsequent UserDB layer
 // which i believe is the userGorm ByRemember
 func (uv *userValidator) ByRemember(token string) (*User, error) {
-	rememberHash := uv.hmac.Hash(token)
-	return uv.UserDB.ByRemember(rememberHash) //
+	user := User{
+		Remember: token,
+	}
+	if err := runUserValFuncs(&user, uv.hmacRemember); err != nil {
+		return nil, err
+	}
+
+	return uv.UserDB.ByRemember(user.RememberHash) //
 }
 
 //Creat provided user and backfill data like the ID, CreatedAt and UpdatedAt fields
 func (uv *userValidator) Create(user *User) error {
-	if err := runUserValFuncs(user, uv.bcryptPassword); err != nil {
-		return err
-	}
 	if user.Remember == "" {
 		token, err := rand.RememberToken()
 		if err != nil {
@@ -163,17 +166,19 @@ func (uv *userValidator) Create(user *User) error {
 		}
 		user.Remember = token
 	}
-	user.RememberHash = uv.hmac.Hash(user.Remember)
+	err := runUserValFuncs(user, uv.bcryptPassword, uv.hmacRemember)
+	if err != nil {
+		return err
+	}
 	return uv.UserDB.Create(user)
 }
 
 // update will hash a remember token if it's provided
 func (uv *userValidator) Update(user *User) error {
-	if err := runUserValFuncs(user, uv.bcryptPassword); err != nil {
+
+	err := runUserValFuncs(user, uv.bcryptPassword, uv.hmacRemember)
+	if err != nil {
 		return err
-	}
-	if user.Remember != "" {
-		user.RememberHash = uv.hmac.Hash(user.Remember)
 	}
 	return uv.UserDB.Update(user)
 }
@@ -201,7 +206,14 @@ func (uv *userValidator) bcryptPassword(user *User) error {
 	user.PasswordHash = string(hashedBytes)
 	user.Password = ""
 	return nil
+}
 
+func (uv *userValidator) hmacRemember(user *User) error {
+	if user.Remember == "" {
+		return nil
+	}
+	user.RememberHash = uv.hmac.Hash(user.Remember)
+	return nil
 }
 
 // underscore tells compiler that this variable will never actually be used
