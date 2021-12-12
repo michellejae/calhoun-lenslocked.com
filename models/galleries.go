d860c64abea69b207126498ea1ab95ab40519448
michelle jae <moyer.mj@gmail.com>
2021-12-12 11:45:57 -0500

    Started implementing the services type and created gallery service interface

--- models/galleries.go
+++ models/galleries.go
@@ -3,8 +3,20 @@ package models
 import "github.com/jinzhu/gorm"
 
 // image container resources that visitors view
-type Galler struct {
+type Gallery struct {
 	gorm.Model
 	UserID uint   `gorm:"not_null;index"`
 	Title  string `gorm:"not_null"`
+}
+
+type GalleryService interface {
+	GalleryDB
+}
+
+type GalleryDB interface {
+	Create(gallyer *Gallery) error
+}
+
+type galleryGorm struct {
+	db *gorm.DB
 }
