2da3684377091e208f645896d60413d36d304295
michelle jae <moyer.mj@gmail.com>
2021-12-23 22:44:48 -0500

    finished implementing redirect with alert

--- views/view.go
+++ views/view.go
@@ -61,10 +61,15 @@ func (v *View) Render(w http.ResponseWriter, r *http.Request, data interface{}) 
 	switch d := data.(type) {
 	case Data:
 		vd = d
+		// do nothing
 	default:
-		data = Data{
+		vd = Data{
 			Yield: data,
 		}
+	}
+	if alert := getAlert(r); alert != nil {
+		vd.Alert = alert
+		clearAlert(w)
 	}
 	vd.User = context.User(r.Context())
 	var buf bytes.Buffer
