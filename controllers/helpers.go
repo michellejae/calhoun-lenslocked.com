37e8b80a8c6e694a9d5e84638035cd2fbf26bf2a
michelle jae <moyer.mj@gmail.com>
2022-01-03 14:34:18 -0800

    implemented form data persistence

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/gorilla/schema"
 )
@@ -11,10 +12,20 @@ func parseForm(r *http.Request, dst interface{}) error {
 	if err := r.ParseForm(); err != nil {
 		return err
 	}
+	return parseValues(r.PostForm, dst)
+}
+
+func parseURLParams(r *http.Request, dst interface{}) error {
+	if err := r.ParseForm(); err != nil {
+		return nil
+	}
+	return parseValues(r.Form, dst)
+}
+func parseValues(values url.Values, dst interface{}) error {
 	// create a decoder with schema package to decode form and save it into our signupform type
 	dec := schema.NewDecoder()
 	dec.IgnoreUnknownKeys(true)
-	if err := dec.Decode(dst, r.PostForm); err != nil {
+	if err := dec.Decode(dst, values); err != nil {
 		return err
 	}
 	return nil
--- controllers/users.go
+++ controllers/users.go
@@ -32,7 +32,9 @@ type Users struct {
 // New is used to render the form where a user can create a new user account
 // Get /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	u.NewView.Render(w, r, nil)
+	var form SignupForm
+	parseURLParams(r, &form)
+	u.NewView.Render(w, r, form)
 
 }
 
@@ -48,6 +50,7 @@ type SignupForm struct {
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var vd views.Data
 	var form SignupForm
+	vd.Yield = &form
 	if err := parseForm(r, &form); err != nil {
 		vd.SetAlert(err)
 		u.NewView.Render(w, r, vd)
